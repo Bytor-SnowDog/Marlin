ada8e3faf0d8ba70a7033ec69bdf8205d7d4da69
Rob <reickmeier@icloud.com>
2023-07-08 14:01:31 -0400

    Update Configuration.h
    Marlin 2.1.2.1 with default configs compiles successfully

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -91,7 +91,7 @@
   #define MOTHERBOARD BOARD_BTT_SKR_V2_0_REV_B // RASCAL 
 #endif
 
-// #define X_MAX_PIN  3 // Creality connects X_MAX to X_MIN(_PIN) motherboard connector
+#define X_MAX_PIN  3 // Creality connects X_MAX to X_MIN(_PIN) motherboard connector
 #define Y_MAX_PIN 14 // Creality connects Y_MAX to Y_MIN(_PIN) motherboard connector
 
 /**
