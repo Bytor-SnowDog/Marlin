477b65ef2aa236654fcc5f0c335961520081fc70
Rob <reickmeier@icloud.com>
2023-07-16 08:53:34 -0400

    Update Configuration.h
    Change to...
    #define MULTIPLE_PROBING 2 // RASCAL default 2
    #define GRID_MAX_POINTS_X 4 // RASCAL default 3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1549,7 +1549,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 3 // RASCAL default 2 
+#define MULTIPLE_PROBING 2 // RASCAL default 2 
 //#define EXTRA_PROBING  1
 
 /**
@@ -1959,7 +1959,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5 // RASCAL default 3
+  #define GRID_MAX_POINTS_X 4 // RASCAL default 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
