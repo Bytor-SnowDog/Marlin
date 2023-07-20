cd3921e98c159c554eb29ddcbd2b4736b9f3a7d0
Rob <reickmeier@icloud.com>
2023-07-20 10:51:55 -0400

    Extrude Quality
    E0 from 1000ma to 1100ma
    Z1 from 650ma to 600ma
    Z2 from 650ma to 600ma

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2762,7 +2762,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z)
-    #define Z_CURRENT       800  // RASCAL default 800, Z banding, almost gone with 1000 without Z_STEPPER_AUTO_ALIGN, With auto align set to 650
+    #define Z_CURRENT       600  // RASCAL default 800, With z stpeer auto align set to 600
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2772,7 +2772,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z2)
-    #define Z2_CURRENT      800 // RASCAL default 800, Z banding, almost gone with 1000 without Z_STEPPER_AUTO_ALIGN, With auto align set to 650
+    #define Z2_CURRENT      600  // RASCAL default 800, With z stpeer auto align set to 600
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    Z_MICROSTEPS
     #define Z2_RSENSE         0.11
@@ -2862,7 +2862,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(E0)
-    #define E0_CURRENT      1000  // RASCAL default 800, Hemera recommends 1000
+    #define E0_CURRENT      1100  // RASCAL default 800, Hemera recommends 1000, trying 1100 for extrude quality
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
