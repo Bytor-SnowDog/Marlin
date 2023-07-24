0cc0bd51f2c080118a501cd7a3c30064aeb1c032
Rob <reickmeier@icloud.com>
2023-07-24 07:23:19 -0400

    Current Changes
    Changed all steppers to 650ma

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2722,7 +2722,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC_CONFIG(X)
-    #define X_CURRENT       600        // (mA) RMS current. Multiply by 1.414 for peak current. // RASCAL default 800
+    #define X_CURRENT       650        // (mA) RMS current. Multiply by 1.414 for peak current. // RASCAL default 800
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11     // Multiplied x1000 for TMC26X
@@ -2742,7 +2742,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Y)
-    #define Y_CURRENT       600  // RASCAL default 800
+    #define Y_CURRENT       650  // RASCAL default 800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2762,7 +2762,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z)
-    #define Z_CURRENT       600  // RASCAL default 800, With Z_STEPPER_AUTO_ALING set to 600 (needs only a couple iterations)
+    #define Z_CURRENT       650  // RASCAL default 800, With Z_STEPPER_AUTO_ALING set to 600 (needs only a couple iterations)
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2772,7 +2772,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z2)
-    #define Z2_CURRENT      600  // RASCAL default 800, With Z_STEPPER_AUTO_ALING set to 600 (needs only a couple iterations)
+    #define Z2_CURRENT      650  // RASCAL default 800, With Z_STEPPER_AUTO_ALING set to 600 (needs only a couple iterations)
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    Z_MICROSTEPS
     #define Z2_RSENSE         0.11
