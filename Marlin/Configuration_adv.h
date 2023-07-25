7d71c667419ba94aebb5ad2c7efc519d590ff18c
Rob <reickmeier@icloud.com>
2023-07-25 16:50:04 -0400

    Update Configuration_adv.h
    X direction layer shift.
    Changed X and Y stepper currents from 650 to 700

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2722,7 +2722,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC_CONFIG(X)
-    #define X_CURRENT       650        // (mA) RMS current. Multiply by 1.414 for peak current. // RASCAL default 800
+    #define X_CURRENT       700        // (mA) RMS current. Multiply by 1.414 for peak current. // RASCAL default 800
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11     // Multiplied x1000 for TMC26X
@@ -2742,7 +2742,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Y)
-    #define Y_CURRENT       650  // RASCAL default 800
+    #define Y_CURRENT       700  // RASCAL default 800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
